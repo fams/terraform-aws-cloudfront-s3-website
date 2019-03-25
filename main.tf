1d62d0f84fb0b369d5c4345edb60f1d3287bc71b
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2019-03-25 17:07:03 -0300

    TLS 2016

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     acm_certificate_arn      = "${var.acm_cert_arn}"
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1"
+    minimum_protocol_version = "TLSv1.1_2016"
   }
   custom_error_response {
     error_code            = 403
